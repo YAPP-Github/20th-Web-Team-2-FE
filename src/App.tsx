06e6656c6c4d13045d90184791cadbfb59ed57ae
Noopy <kimm.jeongs@gmail.com>
2022-05-19 00:28:50 +0900

    Feat: Button 컴포넌트 1차 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import Logo from './logo.svg?component';
-import { Test } from '@/components';
+import { Button } from '@/components/base';
+import { useState } from 'react';
 
 function App() {
+  const onClick = () => console.log('hi');
+
   return (
     <div className="App">
       <header>
         <Logo />
-        <Test />
+        <p> size === 'medium', variant === 'default'  </p>
+        <Button onClick={onClick}>인증번호로 보내기</Button>
+        <p> props === 'boxShadow' </p>
+        <Button boxShadow >인증번호로 보내기</Button>
+        <p> size === 'large' </p>
+        <Button size="large">인증번호로 보내기</Button>
+        <p> size === 'large' variant === 'gray' </p>
+        <Button size="large" variant="gray">응답 수정하기</Button>
+        <p> size === 'large' fontWeight = 700 </p>
+        <Button size="large" fontWeight={700}>다음</Button>
+        <p> size === 'large' variant === 'grayBlack' </p>
+        <Button size="large" variant='grayBlack'>다음</Button>
       </header>
     </div>
   );
