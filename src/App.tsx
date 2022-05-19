98ac13d61de8a64fbb8007ccdd458f03456b5b86
이지호 <cctv1998@naver.com>
2022-05-19 12:44:18 +0900

    Feat: globalStyles 모든 html 태그의 style를 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Logo from './logo.svg?component';
 import { Test } from '@/components';
+import GlobalStyles from './lib/styles/globalStyles';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <header>
         <Logo />
         <Test />
