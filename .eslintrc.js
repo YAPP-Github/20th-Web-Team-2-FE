59d53c835b61d25dcd76467847eb9bbc1bbb1fa2
choisohyun <sohyun4283@naver.com>
2022-05-21 00:55:39 +0900

    Chore: 린트 의존성 및 rules 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
-{
+// eslint-disable-next-line no-undef
+module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -19,9 +20,11 @@
     es2021: true,
   },
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'on',
-    '@typescript-eslint/explicit-function-return-type': 'on',
-    '@typescript-eslint/explicit-module-boundary-types': 'on',
-    '@typescript-eslint/no-explicit-any': 'on',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/no-unescaped-entities': 'off',
   },
-}
+};
