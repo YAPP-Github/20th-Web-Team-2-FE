fefe23eadafbeb85f045dff8f53545c7bd953111
이지호 <cctv1998@naver.com>
2022-05-21 14:56:20 +0900

    Feat: input 컴포넌트 기본 기능 구현

--- src/components/base/Input.tsx
+++ src/components/base/Input.tsx
@@ -1,28 +1,73 @@
 import { palette } from '@/lib/styles/palette';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  width: string;
-  height: string;
+interface InputStyle {
+  /**
+   * focusColor : focus 될 때 border-bottom의 컬러
+   */
+  focusColor?: string;
 }
 
-const InputStyled = styled.input`
+interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement>,
+    InputStyle {
+  /**
+   * width : 가로길이
+   */
+  width?: string;
+
+  /**
+   * height : 세로길이
+   */
+  height?: string;
+
+  /**
+   * isFocus : 랜더링시 해당 input에 focus를 할건지?
+   */
+  isFocus?: boolean;
+}
+
+function Input({
+  width = '312px',
+  height = '38px',
+  focusColor = palette.primary,
+  isFocus = false,
+  ...rest
+}: InputProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
+
+  console.log(focusColor);
+  return (
+    <InputStyled
+      {...rest}
+      height={height}
+      width={width}
+      focusColor={focusColor}
+      isFocus={isFocus}
+    />
+  );
+}
+
+const InputStyled = styled.input<InputProps>`
   width: ${({ width }) => width};
   height: ${({ height }) => height};
   border-bottom: 0.6px solid rgba(0, 0, 0, 0.6);
   padding: 5.5px 12px 5.5px 10px;
   outline: none;
+  transition: all 0.2s linear;
   ::placeholder {
     color: rgba(0, 0, 0, 0.6);
   }
   :focus {
-    border-bottom: 0.6px solid ${palette.primary};
+    border-bottom: 0.6px solid ${({ focusColor }) => focusColor};
   }
 `;
-
-function Input({ width = '312px', height = '38px', ...rest }: InputProps) {
-  return <InputStyled {...rest} />;
-}
 
 export default Input;
