689b1d8f8dfa896f42ccd81adb884ad46efb529d
choisohyun <sohyun4283@naver.com>
2022-05-29 15:29:19 +0900

    Feat: 전체 공통 레이아웃 스타일 추가

--- src/router/Routing.tsx
+++ src/router/Routing.tsx
@@ -6,19 +6,27 @@ import Test from '@/components/base/Test';
 import AuthMail from '@/pages/AuthMail';
 import Survey from '@/pages/Survey';
 import NotFound from '@/pages/NotFound';
+import styled from 'styled-components';
 
 function Routing() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path={Path.LandingPage} element={<Landing />} />
-        <Route path={Path.Component} element={<Test />} />
-        <Route path={Path.AuthMail} element={<AuthMail />} />
-        <Route path={Path.Survey} element={<Survey />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path={Path.LandingPage} element={<Landing />} />
+          <Route path={Path.Component} element={<Test />} />
+          <Route path={Path.AuthMail} element={<AuthMail />} />
+          <Route path={Path.Survey} element={<Survey />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }
+
+const Layout = styled.main`
+  margin: 0 16px;
+  height: 100%;
+`;
 
 export default Routing;
