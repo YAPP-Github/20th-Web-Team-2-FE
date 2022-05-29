95a452fb21aaa2f961cee3875f08a82f9e8ba224
choisohyun <sohyun4283@naver.com>
2022-05-29 15:31:34 +0900

    Feat: 설문, 인증 템플릿 추가

---
+++ src/components/survey/SurveyTemplate.tsx
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { ReactNode } from 'react';
+import { Button } from '@/components/base';
+import ProgressBar from '@/components/base/ProgressBar';
+
+interface SurveyTemplateProps {
+  children: ReactNode;
+  hasProgressBar?: boolean;
+  disableNext: boolean;
+}
+
+const SurveyTemplate = ({ children, hasProgressBar = true, disableNext }: SurveyTemplateProps) => {
+  return (
+    <>
+      <HeaderWrapper>
+        <Logo to="/">외딴썸</Logo>
+      </HeaderWrapper>
+      {children}
+      <NavigationWrapper>
+        {hasProgressBar && <ProgressBar currStep={1} totalStep={10} />}
+        <ButtonWrapper>
+          <Button size="large" variant="gray">
+            이전
+          </Button>
+          <Button
+            onClick={() => console.log('aa')}
+            size="large"
+            disabled={disableNext}
+            variant={disableNext ? 'gray' : 'default'}
+            fontWeight={disableNext ? 400 : 700}
+          >
+            다음
+          </Button>
+        </ButtonWrapper>
+      </NavigationWrapper>
+    </>
+  );
+};
+
+const HeaderWrapper = styled.header`
+  height: 56px;
+  padding: 20px 0;
+  margin-bottom: 26px;
+`;
+
+const Logo = styled(Link)`
+  font-family: SangjuGotgam, Pretendard Variable, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI',
+    'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 125%;
+  text-align: center;
+  color: rgba(0, 0, 0, 0.8);
+`;
+
+const NavigationWrapper = styled.div`
+  position: fixed;
+  width: calc(100% - 32px);
+  bottom: 38px;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  width: calc(50% - 4px);
+  gap: 8px;
+`;
+
+export default SurveyTemplate;
