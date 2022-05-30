90897859f22c885d0a193cb61f464d8dd23eff64
Jiyoung Kim <kimm.jeongs@gmail.com>
2022-05-31 01:28:29 +0900

    feat: Title 공통 컴포넌트 구현

---
+++ src/lib/styles/styledComponents.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 30px;
+  letter-spacing: -0.02em;
+  color: rgba(0, 0, 0, 0.9);
+`;
