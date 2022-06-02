ca8b5a48ef39ee58a07bbfa442b22ec7a230958f
Jiyoung Kim <kimm.jeongs@gmail.com>
2022-06-02 23:14:04 +0900

    Feat: 공통 SubTitle 구현

--- src/lib/styles/styledComponents.ts
+++ src/lib/styles/styledComponents.ts
@@ -7,3 +7,11 @@ export const Title = styled.h1`
   letter-spacing: -0.02em;
   color: rgba(0, 0, 0, 0.9);
 `;
+
+export const SubTitle = styled.h2`
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 125%;
+  color: rgba(0, 0, 0, 0.8);
+  padding-bottom: 18px;
+`;
