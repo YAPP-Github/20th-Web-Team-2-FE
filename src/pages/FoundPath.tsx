3dcfc29b32b7725847b40ec2a8b7530e53402a73
이지호 <cctv1998@naver.com>
2022-06-03 18:13:32 +0900

    Feat: 알게된 경로 설문 check 데이터 담아서 전달

--- src/pages/FoundPath.tsx
+++ src/pages/FoundPath.tsx
@@ -1,22 +1,20 @@
-import { useState } from 'react';
 import SurveyTemplate from '@/components/survey/SurveyTemplate';
 import { Title } from '@/lib/styles/styledComponents';
 import CheckBox from '@/components/base/CheckBox';
 import { FormWrapper } from './AuthMail';
+import useFoundPathCheck from '@/hooks/agreement/useFoundPathCheck';
 
 const FoundPath = () => {
-  const BTNS = ['페이스북', '인스타그램', '카카오단톡방', '카톡플친', '지인추천', '기타 커뮤니티'];
-  const [cantMoveNext, setCantMoveNext] = useState(true);
-  console.log(cantMoveNext);
+  const { pathCheckList, onChangeCheck } = useFoundPathCheck();
   return (
-    <SurveyTemplate disableNext={cantMoveNext} hasProgressBar={false}>
+    <SurveyTemplate disableNext={false} hasProgressBar={false}>
       <Title>
         외딴썸을 처음 알게 된 <br />
         경로를 알려주세요.
       </Title>
       <FormWrapper>
-        {BTNS.map((btn) => (
-          <CheckBox key={btn} text={btn} onChange={() => setCantMoveNext((prev) => !prev)} />
+        {pathCheckList.map(({ text, checked, name }) => (
+          <CheckBox key={text} text={text} checked={checked} onChange={onChangeCheck} name={name} impotrant={checked ? true : false} />
         ))}
       </FormWrapper>
     </SurveyTemplate>
