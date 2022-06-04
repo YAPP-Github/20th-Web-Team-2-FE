48decbd89b32c9b199942c3f9b9bcae749a5d8c2
이지호 <cctv1998@naver.com>
2022-06-04 20:07:16 +0900

    Feat: PageLayout 설정

--- src/router/Routing.tsx
+++ src/router/Routing.tsx
@@ -9,26 +9,34 @@ import styled from 'styled-components';
 import LandingPage from '@/pages/LandingPage';
 import FoundPath from '@/pages/FoundPath';
 import Agreement from '@/pages/Agreement';
+import InputKaKao from '@/pages/InputKaKao';
 
 function Routing() {
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path={Path.LandingPage} element={<LandingPage />} />
-          <Route path={Path.Component} element={<Test />} />
-          <Route path={Path.AuthMail} element={<AuthMail />} />
-          <Route path={Path.Survey} element={<Survey />} />
-          <Route path={Path.Survey16} element={<FoundPath />} />
-          <Route path={Path.Survey17} element={<Agreement />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Layout>
+      <PageLayout>
+        <PageWrapper>
+          <Routes>
+            <Route path={Path.LandingPage} element={<LandingPage />} />
+            <Route path={Path.Component} element={<Test />} />
+            <Route path={Path.AuthMail} element={<AuthMail />} />
+            <Route path={Path.Survey} element={<Survey />} />
+            <Route path={Path.Survey16} element={<FoundPath />} />
+            <Route path={Path.Survey17} element={<Agreement />} />
+            <Route path={Path.Survey18} element={<InputKaKao />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </PageWrapper>
+      </PageLayout>
     </BrowserRouter>
   );
 }
-
-const Layout = styled.main`
+const PageLayout = styled.div`
+  margin: 0 auto;
+  max-width: 344px;
+  height: 100%;
+`;
+const PageWrapper = styled.main`
   margin: 0 16px;
   height: 100%;
 `;
