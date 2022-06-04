08886ed445a9537eb78d9b2c7383bb0b89eec42e
Jiyoung Kim <kimm.jeongs@gmail.com>
2022-06-05 05:56:10 +0900

    fix: 배포 오류 수정

--- src/router/Path.ts
+++ src/router/Path.ts
@@ -8,7 +8,7 @@ enum Path {
   OurDepartmentsAverageHeightSurvey = 'our-dprt-height',
   AvoidUniversitiesSurvey = 'avoid-univ',
   PreferUniversitiesSurvey = 'prefer-univ',
-  PreferAgeHeightSurvey = 'prefer-age-height',
+  PreferAverageAgeHeightSurvey = 'prefer-age-height',
   PreferDepartmentsSurvey = 'prefer-dprt',
   MindsetSurvey = 'mindset',
   PlaySurvey = 'paly',
