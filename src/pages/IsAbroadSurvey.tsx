4be9bd8c5a6e153722b216f4807f35d411f477a0
이지호 <cctv1998@naver.com>
2022-06-05 06:38:01 +0900

    Chore: progressbar 사용 허가

--- src/pages/IsAbroadSurvey.tsx
+++ src/pages/IsAbroadSurvey.tsx
@@ -4,15 +4,25 @@ import { Title } from '@/lib/styles/styledComponents';
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { FormWrapper } from './AuthMail';
+import useMeetingNavigate from '@/hooks/common/useMeetingNavigate';
+import Path from '@/router/Path';
 
 const IsAbroadSurvey = () => {
+  const meetingNavigate = useMeetingNavigate();
   const [isAbroad, setIsAbroad] = useState('domestic');
   const onChangeOption = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { id } = e.target;
     setIsAbroad(id);
   };
   return (
-    <SurveyTemplate disableNext={true} hasProgressBar={false} currStep={3} totalStep={15}>
+    <SurveyTemplate
+      disableNext={true}
+      hasProgressBar={true}
+      currStep={3}
+      totalStep={15}
+      handlePrevClick={() => meetingNavigate(Path.PlaySurvey)}
+      handleNextClick={() => meetingNavigate(Path.DomesticAreasSurvey)}
+    >
       <Title>
         지금 한국이신가요? <br />
         해외이신가요?
