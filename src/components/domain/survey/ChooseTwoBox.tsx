948f90bc2267f6c2deed939bf482cdd8879bcd7d
이지호 <cctv1998@naver.com>
2022-06-05 06:05:23 +0900

    Refactor: genderbox => chooseTwoBox 재활용 가능하게 변경

---
+++ src/components/domain/survey/ChooseTwoBox.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled from 'styled-components';
+import { SubTitle } from '@/lib/styles/styledComponents';
+import { ChoiceButton } from '@/components/base';
+import { ButtonSizes } from '@/utils/buttons';
+
+interface ChooseTwoBoxProps {
+  selectedOption: string;
+  onChangeOption: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  children: React.ReactNode;
+  items: Array<{ id: string; text: string }>;
+  size?: ButtonSizes;
+  height?: 38 | 48 | 70 | 100;
+}
+
+const ChooseTwoBox = ({ selectedOption, onChangeOption, children, items, size = 'medium', ...rest }: ChooseTwoBoxProps) => {
+  return (
+    <Container>
+      <SubTitle>{children}</SubTitle>
+      <ButtonWrapper>
+        {items.map((item) => (
+          <ChoiceButton
+            {...rest}
+            name="typeOfmeeting"
+            size={size}
+            variant="grayBlack"
+            id={item.id}
+            onChange={onChangeOption}
+            key={item.id}
+            checked={item.id === selectedOption}
+          >
+            {item.text}
+          </ChoiceButton>
+        ))}
+      </ButtonWrapper>
+    </Container>
+  );
+};
+
+const Container = styled.section`
+  width: 100%;
+  margin-top: 67px;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  gap: 8px;
+`;
+
+export default ChooseTwoBox;
