290936fc93ad67b8a90667936af11c9fca9434fd
Jiyoung Kim <kimm.jeongs@gmail.com>
2022-06-05 05:24:13 +0900

    fix: 안쓰는 _ 속성 삭제

--- src/components/domain/survey/HeightBox.tsx
+++ src/components/domain/survey/HeightBox.tsx
@@ -24,7 +24,7 @@ const HeightBox = ({ setHeightOption, setMultiHeightOption, children, isMulti = 
   }
 
   const handleSimpleChange = useCallback(
-    ({ _, max }: Ranges) => {
+    ({ max }: Ranges) => {
       if (!isMulti && setHeightOption) {
         setHeightOption(max);
       }
