ae20a1a0c084c5f23bec44315d36b3dd94c570f1
이지호 <cctv1998@naver.com>
2022-06-04 19:54:22 +0900

    Feat: footer 사업자 정보 추가

--- src/assets/img/index.ts
+++ src/assets/img/index.ts
@@ -3,3 +3,5 @@ export { default as RadiousLogo } from './RadiousLogo.png';
 export { default as StringLogo } from './StringLogo.png';
 export { default as SearchIcon } from './SearchIcon.png';
 export { default as DeleteIcon } from './DeleteIcon.png';
+export { default as KakaoIcon } from './KakaoIcon.png';
+export { default as InstagramIcon } from './InstagramIcon.png';
