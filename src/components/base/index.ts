88ff4c51a4c1895829276e8b78b6b70227421588
Jiyoung Kim <kimm.jeongs@gmail.com>
2022-06-04 19:35:05 +0900

    refactor: Button 풀더말고 컴포넌트로 가져옴

--- src/components/base/index.ts
+++ src/components/base/index.ts
@@ -1,4 +1,4 @@
-export { default as Button } from './Button/Button';
+export { default as Button } from './Button';
 export { default as ChoiceButton } from './ChoiceButton';
 export { default as Input } from './Input';
 export { default as Modal } from './Modal';
