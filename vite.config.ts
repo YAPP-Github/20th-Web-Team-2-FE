bfeb70b8ef208d78cf3264a8e6110e57aebf3396
이지호 <cctv1998@naver.com>
2022-06-12 18:26:38 +0900

    Fix: 오류때문에 vite plugin 임시로 제거

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from '@honkhonk/vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -23,9 +22,9 @@ export default defineConfig({
     }),
     tsconfigPaths(),
     svgr({ keepEmittedAssets: true }),
-    createHtmlPlugin({
-      minify: true,
-      template: 'public/index.html',
-    }),
+    // createHtmlPlugin({
+    //   minify: true,
+    //   template: 'public/index.html',
+    // }),
   ],
 });
