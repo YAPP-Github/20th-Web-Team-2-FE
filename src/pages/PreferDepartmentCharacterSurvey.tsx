96519142e2a3cb3893c1111cd454e5c8549a2635
이지호 <cctv1998@naver.com>
2022-06-15 17:04:13 +0900

    Chore: 잘못된 네비게이트 위치변경

--- src/pages/PreferDepartmentCharacterSurvey.tsx
+++ src/pages/PreferDepartmentCharacterSurvey.tsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import { ChooseFourBox, SurveyTemplate } from '@/components/domain/survey';
 import { ChooseFourBoxItemProps } from '@/components/domain/survey/ChooseFourBox';
-import { useMeetingNavigate } from '@/hooks/common/useMeetingNavigate';
+import { useDatingNavigate } from '@/hooks/common/useMeetingNavigate';
 import Path from '@/router/Path';
 import { PREFER_DEPARTMENT_ITEMS } from '@/types/constants/department';
 import { PREFER_CHARACTER_ITEMS } from '@/types/constants/charater';
 
 const PreferDepartmentCharacterSurvey = () => {
-  const meetingNavigate = useMeetingNavigate();
+  const datingNavigate = useDatingNavigate();
   const [checkedMultiOption, setMultiCheckedOption] = useState<ChooseFourBoxItemProps[]>(PREFER_DEPARTMENT_ITEMS);
   const [checkedMultiOption2, setMultiCheckedOption2] = useState<ChooseFourBoxItemProps[]>(PREFER_CHARACTER_ITEMS);
   return (
@@ -15,15 +15,15 @@ const PreferDepartmentCharacterSurvey = () => {
       disableNext={!checkedMultiOption || !checkedMultiOption2}
       currStep={9}
       totalStep={11}
-      handleNextClick={() => meetingNavigate(Path.PreferBodyDateCountSurvey)}
-      handlePrevClick={() => meetingNavigate(Path.PreferAgeHeightSurvey)}
+      handleNextClick={() => datingNavigate(Path.PreferBodyDateCountSurvey)}
+      handlePrevClick={() => datingNavigate(Path.PreferAgeHeightSurvey)}
     >
       <ChooseFourBox
         isMulti
         items={PREFER_DEPARTMENT_ITEMS}
         checkedMultiOption={checkedMultiOption}
         setMultiCheckedOption={setMultiCheckedOption}
-        top={97}
+        top={31}
       >
         선호하는 학과를 모두 알려주세요.
       </ChooseFourBox>
@@ -32,7 +32,7 @@ const PreferDepartmentCharacterSurvey = () => {
         items={PREFER_DEPARTMENT_ITEMS}
         checkedMultiOption={checkedMultiOption2}
         setMultiCheckedOption={setMultiCheckedOption2}
-        top={97}
+        top={31}
       >
         선호하는 성격을 모두 알려주세요.
       </ChooseFourBox>
