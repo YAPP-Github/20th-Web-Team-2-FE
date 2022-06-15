918eb01b575f242a0a729ca75307e14ccdf70f40
이지호 <cctv1998@naver.com>
2022-06-15 17:09:46 +0900

    Chore: Preferbodydatecountsurvey 배열 변경

--- src/pages/PreferBodyDateCountSurvey.tsx
+++ src/pages/PreferBodyDateCountSurvey.tsx
@@ -1,30 +1,24 @@
 import React, { useState } from 'react';
 import { ChooseFourBox, SurveyTemplate } from '@/components/domain/survey';
 import { ChooseFourBoxItemProps } from '@/components/domain/survey/ChooseFourBox';
-import { useMeetingNavigate } from '@/hooks/common/useMeetingNavigate';
+import { useDatingNavigate } from '@/hooks/common/useMeetingNavigate';
 import Path from '@/router/Path';
-import { PREFER_DEPARTMENT_ITEMS } from '@/types/constants/department';
 import { PREFER_DCOUNT_ITEMS } from '@/types/constants/dcount';
+import { PREFER_BODY_ITEMS } from '@/types/constants/body';
 
 const PreferBodyDateCountSurvey = () => {
-  const meetingNavigate = useMeetingNavigate();
-  const [checkedMultiOption, setMultiCheckedOption] = useState<ChooseFourBoxItemProps[]>(PREFER_DEPARTMENT_ITEMS);
+  const datingNavigate = useDatingNavigate();
+  const [checkedMultiOption, setMultiCheckedOption] = useState<ChooseFourBoxItemProps[]>(PREFER_BODY_ITEMS);
   const [checkedCharacterOption, setCharacterCheckedOption] = useState('');
   return (
     <SurveyTemplate
       disableNext={!checkedMultiOption && !checkedCharacterOption}
       currStep={10}
       totalStep={11}
-      handlePrevClick={() => meetingNavigate(Path.PreferAgeHeightSurvey)}
+      handlePrevClick={() => datingNavigate(Path.PreferAgeHeightSurvey)}
     >
-      <ChooseFourBox
-        isMulti
-        items={PREFER_DEPARTMENT_ITEMS}
-        checkedMultiOption={checkedMultiOption}
-        setMultiCheckedOption={setMultiCheckedOption}
-        top={31}
-      >
-        선호하는 학과를 모두 알려주세요.
+      <ChooseFourBox isMulti items={PREFER_BODY_ITEMS} checkedMultiOption={checkedMultiOption} setMultiCheckedOption={setMultiCheckedOption} top={31}>
+        선호하는 체형을 모두 선택해주세요.
       </ChooseFourBox>
       <ChooseFourBox items={PREFER_DCOUNT_ITEMS} checkedOption={checkedCharacterOption} setCheckedOption={setCharacterCheckedOption} top={45}>
         선호하는 상대방의 연애 횟수를 선택해주세요.
--- src/types/constants/body.ts
+++ src/types/constants/body.ts
@@ -24,3 +24,30 @@ export const MYBODY_ITEMS = [
     checked: false,
   },
 ] as const;
+
+export const PREFER_BODY_ITEMS = [
+  {
+    id: 'SKINNY',
+    text: '마른 편',
+    name: 'preferBodies',
+    checked: false,
+  },
+  {
+    id: 'SLIM',
+    text: '슬림 탄탄',
+    name: 'preferBodies',
+    checked: false,
+  },
+  {
+    id: 'MUSCULAR',
+    text: '근육',
+    name: 'preferBodies',
+    checked: false,
+  },
+  {
+    id: 'CHUBBY',
+    text: '통통한',
+    name: 'preferBodies',
+    checked: false,
+  },
+];
