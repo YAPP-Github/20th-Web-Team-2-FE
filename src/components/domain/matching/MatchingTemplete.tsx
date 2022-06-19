cbd700faca87c38bc3de8cfad1844831138ac2b0
이지호 <cctv1998@naver.com>
2022-06-19 13:49:24 +0900

    Feat: matching 템플릿

--- src/components/domain/matching/MatchingTemplete.tsx
+++ src/components/domain/matching/MatchingTemplete.tsx
@@ -1,7 +1,67 @@
-import React from 'react';
+import React, { MouseEventHandler, ReactNode, useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { Button } from '@/components/base';
+import UserHeader from './UserHeader';
+import { useLocation } from 'react-router-dom';
 
-const MatchingTemplete = () => {
-  return <div>MatchingTemplete</div>;
+interface MatchingTemplateProps {
+  children: ReactNode;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  IsDisable: boolean;
+}
+const MatchingTemplete = ({ children, handleClick, IsDisable }: MatchingTemplateProps) => {
+  const location = useLocation();
+  const [type, setType] = useState('meeting');
+  useEffect(() => {
+    location.pathname.includes('meeting') ? setType('meeting') : setType('dating');
+  }, [location]);
+
+  return (
+    <TemplateBlock>
+      <UserHeader />
+      {children}
+      <ButtonWrapper>
+        <TypeButton onClick={handleClick} size="medium" variant={type === 'meeting' ? 'default' : 'gray'} fontWeight={type === 'dating' ? 700 : 400}>
+          미팅
+        </TypeButton>
+        <TypeButton onClick={handleClick} size="medium" variant={type === 'dating' ? 'default' : 'gray'} fontWeight={type === 'dating' ? 700 : 400}>
+          소개팅
+        </TypeButton>
+      </ButtonWrapper>
+      <NavigationWrapper>
+        <ButtonWrapper>
+          <Button
+            onClick={handleClick}
+            size="medium"
+            disabled={IsDisable}
+            variant={IsDisable ? 'gray' : 'default'}
+            fontWeight={IsDisable ? 400 : 700}
+          >
+            다음
+          </Button>
+        </ButtonWrapper>
+      </NavigationWrapper>
+    </TemplateBlock>
+  );
 };
+const TemplateBlock = styled.section`
+  position: relative;
+  height: 100%;
+`;
+
+const NavigationWrapper = styled.div`
+  position: absolute;
+  width: 100%;
+  bottom: 38px;
+`;
+const ButtonWrapper = styled.div`
+  display: flex;
+  gap: 8px;
+`;
+const TypeButton = styled(Button)`
+  margin: 4px;
+  width: 80px;
+  height: 38px;
+`;
 
 export default MatchingTemplete;
