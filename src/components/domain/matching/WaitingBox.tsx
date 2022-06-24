3a132ba8f8191c48124d733a014d362b3df633f5
이지호 <cctv1998@naver.com>
2022-06-24 22:34:56 +0900

    Feat: WatiaingBox

---
+++ src/components/domain/matching/WaitingBox.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Waiting } from '@/assets/img';
+import styled from 'styled-components';
+
+function WaitingBox() {
+  return (
+    <>
+      <MatchingImg src={Waiting} alt="" />
+      <StringEle>
+        <strong>매칭은 매일밤 11시에 이루어집니다.</strong>
+        <br />
+        매칭이 되면 카톡과 이메일로 링크를 보내드릴게요.
+      </StringEle>
+      <MarginTopEle>
+        랜덤매칭으로 이상형 반영없이 <br /> 바로 매칭받을 수 있어요
+      </MarginTopEle>
+    </>
+  );
+}
+
+export const MatchingImg = styled.img`
+  width: 140px;
+  margin-bottom: 20px;
+`;
+export const StringEle = styled.p`
+  line-height: 26px;
+`;
+export const MarginTopEle = styled.p`
+  margin-top: 50px;
+`;
+
+export default WaitingBox;
