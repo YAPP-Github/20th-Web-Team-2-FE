613705231df8cf6e4fd29f8c6a60b8fefa432434
이지호 <cctv1998@naver.com>
2022-06-24 22:34:24 +0900

    Feat: CompleteBox

---
+++ src/components/domain/matching/CompleteBox.tsx
@@ -0,0 +1,18 @@
+import { Complete } from '@/assets/img';
+import React from 'react';
+import { MatchingImg, StringEle } from './WaitingBox';
+
+function CompleteBox() {
+  return (
+    <>
+      <MatchingImg src={Complete} alt="" />
+      <StringEle>
+        <strong>양쪽 결제 확인 후 5월 23일 오후 10시에</strong>
+        <br />
+        카톡 아이디가 전달됩니다.
+      </StringEle>
+    </>
+  );
+}
+
+export default CompleteBox;
