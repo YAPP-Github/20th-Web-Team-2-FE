e2e906e82f73d91e414970034fbfa41fd48548bb
Jiyoung Kim <kimm.jeongs@gmail.com>
2022-06-27 23:56:55 +0900

    feat: kakaoId 세션 추가

--- src/pages/KakaoIdSurvey.tsx
+++ src/pages/KakaoIdSurvey.tsx
@@ -8,21 +8,30 @@ import styled from 'styled-components';
 import { FormWrapper } from './AuthMail';
 import Path from '@/router/Path';
 import { useMeetingNavigate } from '@/hooks/common/useMeetingNavigate';
+import { useMeetingSessionState } from '@/hooks/common';
 
 const KakaoIdSurvey = () => {
   const meetingNavigate = useMeetingNavigate();
   const [isModal, onToggleModal] = useToggle();
-  const [kakaoID, setkakaoID] = useState('');
+  const [kakaoId, setkakaoId] = useState('');
+  const { initMeetingState, setMeetingData } = useMeetingSessionState();
+
+  const handleNextClick = () => {
+    alert('설문이 완료되었습니다. 정식 출시가 되면 매칭을 시작할 수 있습니다. 조금만 기다려 주세요.');
+    if (initMeetingState) {
+      setMeetingData({ ...initMeetingState, kakaoId });
+    }
+  };
 
   return (
     <>
       <SurveyTemplate
-        disableNext={false}
+        disableNext={!kakaoId}
         hasProgressBar={true}
         currStep={14}
         totalStep={14}
         handlePrevClick={() => meetingNavigate(Path.AgreementSurvey)}
-        handleNextClick={() => alert('설문이 완료되었습니다. 정식 출시가 되면 매칭을 시작할 수 있습니다. 조금만 기다려 주세요.')}
+        handleNextClick={handleNextClick}
       >
         <Title>
           카톡 아이디를 <br />
@@ -37,10 +46,10 @@ const KakaoIdSurvey = () => {
             isFocus
             variant="filled"
             placeholder="카톡 아이디"
-            value={kakaoID}
+            value={kakaoId}
             height="48px"
             onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-              setkakaoID(e.target.value);
+              setkakaoId(e.target.value);
             }}
           />
           <Explane>※'카톡 ID 검색 허용'으로 되어있어야 합니다.</Explane>
@@ -54,7 +63,7 @@ const KakaoIdSurvey = () => {
           width={200}
           height={140}
           bottonName="확인"
-          title={kakaoID}
+          title={kakaoId}
           text="　카톡아이디를 확인해 주세요.　상대에게 보여질 아아디입니다!"
           onToggleModal={onToggleModal}
           onClick={() => {
