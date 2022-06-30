6d70b63405399f869e5ec0c5585a6eb2e97fd206
이지호 <cctv1998@naver.com>
2022-06-30 15:12:21 +0900

    Design: sidebar header

--- src/components/header/MenuBlock.tsx
+++ src/components/header/MenuBlock.tsx
@@ -1,3 +1,4 @@
+import { Logo } from '@/assets/img';
 import { palette } from '@/lib/styles/palette';
 import React from 'react';
 import styled from 'styled-components';
@@ -6,10 +7,21 @@ interface MenuBlockProps {
   isMenu: boolean;
   onToggleMenu: () => void;
 }
+const TempUserData = {
+  email: 'coffee123@naver.com',
+};
 function MenuBlock({ isMenu, onToggleMenu }: MenuBlockProps) {
   return (
     <>
-      <NavBarBlock isMenu={isMenu}>asdasd</NavBarBlock>
+      <NavBarBlock isMenu={isMenu}>
+        <SidebarHeader>
+          <SiteLogo src={Logo} alt="사이트 로고" />
+          <UserBox>
+            <div>{TempUserData.email}</div>
+            <div className="univ">Boston University</div>
+          </UserBox>
+        </SidebarHeader>
+      </NavBarBlock>
       <NavBackground onClick={onToggleMenu} isMenu={isMenu} />
     </>
   );
@@ -27,17 +39,40 @@ const NavBackground = styled.div<{ isMenu: boolean }>`
   ${({ isMenu }) => (isMenu ? 'display: block' : 'display: none')}
 `;
 
-const NavBarBlock = styled.div<{ isMenu: boolean }>`
+const NavBarBlock = styled.section<{ isMenu: boolean }>`
   width: 100%;
   max-width: 300px;
   position: fixed;
   height: 100vh;
   top: 0;
   left: 0;
-  background: ${palette.primary};
+  background: ${palette.white};
   z-index: 9000;
+  padding: 16px;
   ${({ isMenu }) => (isMenu ? 'visibility: visible' : 'visibility : hidden')};
   transition: all 0.2s ease-in-out;
   transform: translateX(${({ isMenu }) => (isMenu ? 'calc(100vw - 300px)' : '100vw')});
+`;
+const SidebarHeader = styled.header`
+  display: flex;
+  margin-bottom: 20px;
+`;
+
+const SiteLogo = styled.img`
+  width: 45px;
+  border-radius: 100%;
+  padding-right: 6px;
+`;
+const UserBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-left: 4px;
+  font-weight: 700;
+  .univ {
+    font-size: 14px;
+    font-weight: 400;
+    color: ${palette.grayDark};
+  }
 `;
 export default MenuBlock;
