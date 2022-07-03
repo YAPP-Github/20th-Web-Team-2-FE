f323f5a42c115c442dfebc225508c024e969c193
이지호 <cctv1998@naver.com>
2022-07-03 22:51:14 +0900

    Chore: apiClient host VITE_SERVER_KEY로 변경

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const host = import.meta.env.VITE_KAKAO_JAVASCRIPT_KEY;
+const host = import.meta.env.VITE_SERVER_URL;
 const apiClient = axios.create({
   baseURL: host,
   withCredentials: false,
