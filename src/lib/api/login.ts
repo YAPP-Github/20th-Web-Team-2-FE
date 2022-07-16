48a30a6a880b8ed866c0ebf277cc4139b1cc19c1
Jiyoung Kim <kimm.jeongs@gmail.com>
2022-07-16 17:43:37 +0900

    feat: 임시로 join 후 login post

--- src/lib/api/login.ts
+++ src/lib/api/login.ts
@@ -1,6 +1,6 @@
 import apiClient from './index';
 import { LoginResponse, LoginRequest } from '@/types/user';
-
+import { AxiosResponse } from 'axios';
 /*
   @desc
   임시 아이디 비번: test1
@@ -8,12 +8,12 @@ import { LoginResponse, LoginRequest } from '@/types/user';
 
 export const postLogin = async (payload: LoginRequest): Promise<LoginResponse | undefined> => {
   try {
-    const res = await apiClient.post('/login', payload);
-
-    if (res.status !== 200 && res.status !== 201) {
-      throw new Error(res.data.message);
+    const joinRes = await apiClient.post<AxiosResponse>('/join', payload);
+    if (joinRes.status === 200) {
+      const res = await apiClient.post('/login', payload);
+      return res.data;
     }
-    return res.data;
+    throw new Error(joinRes.statusText);
   } catch (e) {
     console.error(e);
   }
