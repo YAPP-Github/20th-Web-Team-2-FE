3aba636b81172fa8e392fd1a52f95734484c54df
Jiyoung Kim <kimm.jeongs@gmail.com>
2022-07-20 23:02:06 +0900

    comment: 주석 변경

--- src/lib/api/login.ts
+++ src/lib/api/login.ts
@@ -2,7 +2,7 @@ import apiClient from './index';
 import { LoginResponse, LoginRequest } from '@/types/user';
 /*
   @desc
-  임시 아이디 비번: test4
+  임시 아이디 비번: test1
 */
 
 export const postLogin = async (payload: LoginRequest): Promise<LoginResponse | undefined> => {
