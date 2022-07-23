1f473a85d83bc3f6ad0c9060e00ac421d35657b4
Jiyoung Kim <kimm.jeongs@gmail.com>
2022-07-23 20:39:00 +0900

    feat: (없을 시 '기타' 입력 ), 학교 추가하기 문구 추가

--- src/pages/OurUniversitiesSurvey.tsx
+++ src/pages/OurUniversitiesSurvey.tsx
@@ -6,6 +6,8 @@ import Path from '@/router/Path';
 import { useMeetingNavigate } from '@/hooks/common/useNavigate';
 import { useMeetingSessionState } from '@/hooks/common';
 import useUnivLoad from '@/hooks/survey/useUnivLoad';
+import styled from 'styled-components';
+import { palette } from '@/lib/styles/palette';
 
 const OurUniversitiesSurvey = () => {
   const meetingNavigate = useMeetingNavigate();
@@ -49,14 +51,37 @@ const OurUniversitiesSurvey = () => {
         모두 입력해주세요.
       </Title>
       <SearchSelector
-        placeholder="학교를 검색하세요."
+        placeholder="학교를 검색하세요. (없을 시 '기타' 입력)"
         searchData={univs}
         selectedResults={ourUniversities}
         setSelectedResults={setOurUniversities}
         max={maxUniv}
       />
+      <Info>
+        학교가 없다고 나오나요?{' '}
+        <Anchor
+          target="_blank"
+          href="https://docs.google.com/forms/d/e/1FAIpQLScTbXoWfKIUtgInYvEaeqDQofvQalNndwcbHMR9F4s5al0v1A/viewform?usp=sf_link"
+        >
+          학교 추가하기
+        </Anchor>
+      </Info>
     </SurveyTemplate>
   );
 };
+
+const Anchor = styled.a`
+  font-weight: 700;
+  text-decoration: underline;
+  color: ${palette.primary};
+  margin-bottom: 30px;
+`;
+
+const Info = styled.span`
+  position: absolute;
+  bottom: 200px;
+  font-size: 12px;
+  font-weight: 300;
+`;
 
 export default OurUniversitiesSurvey;
