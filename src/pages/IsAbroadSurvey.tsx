17f050881358496c55c42b142acb3d0b0c225dd5
이지호 <cctv1998@naver.com>
2022-07-30 22:41:30 +0900

    Feat: IsAbroadSurvey 수정하기 추가

--- src/pages/IsAbroadSurvey.tsx
+++ src/pages/IsAbroadSurvey.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-children-prop */
-import React, { useState, useMemo } from 'react';
+import React, { useState } from 'react';
 import { ChooseTwoBox, SurveyTemplate } from '@/components/domain/survey';
 import { Title } from '@/lib/styles/styledComponents';
 import styled from 'styled-components';
@@ -8,11 +8,14 @@ import { useMeetingNavigate, useDatingNavigate } from '@/hooks/common/useNavigat
 import Path from '@/router/Path';
 import { COUNTRY_ITEMS } from '@/types/constants/area';
 import { useMeetingSessionState, useDatingSessionState } from '@/hooks/common';
-import { useMatch } from 'react-router-dom';
+import { useMatch, useNavigate } from 'react-router-dom';
 import { type Location } from '@/types/meeting';
+import useUpdateSurvey from '@/hooks/survey/useUpdateSurvey';
 
 const IsAbroadSurvey = () => {
   const matchMeeting = useMatch('/meeting/*');
+  const navigate = useNavigate();
+  const { isUpdate, onUpdateMeetingSurvey, onUpdateDatingSurvey } = useUpdateSurvey();
   const meetingNavigate = matchMeeting ? useMeetingNavigate() : useDatingNavigate();
   const { initMeetingState, setMeetingData } = useMeetingSessionState();
   const { initDatingState, setDatingData } = useDatingSessionState();
@@ -30,13 +33,20 @@ const IsAbroadSurvey = () => {
   };
 
   const handleNextClick = () => {
-    if (initMeetingState) {
+    if (isUpdate) {
       matchMeeting
-        ? setMeetingData({ ...initMeetingState, isAbroad: isAbroad === 'ABROAD' ? true : false })
-        : setDatingData({ ...initDatingState, isAbroad: isAbroadDating === 'ABROAD' ? true : false });
-    }
+        ? onUpdateMeetingSurvey({ ...initMeetingState, isAbroad: isAbroad === 'ABROAD' ? true : false })
+        : onUpdateDatingSurvey({ ...initDatingState, isAbroad: isAbroadDating === 'ABROAD' ? true : false });
+      navigate(Path.MatchingDating);
+    } else {
+      if (initMeetingState) {
+        matchMeeting
+          ? setMeetingData({ ...initMeetingState, isAbroad: isAbroad === 'ABROAD' ? true : false })
+          : setDatingData({ ...initDatingState, isAbroad: isAbroadDating === 'ABROAD' ? true : false });
+      }
 
-    meetingNavigate(ISABOARD === 'ABROAD' ? Path.AbroadAreasSurvey : Path.DomesticAreasSurvey);
+      meetingNavigate(ISABOARD === 'ABROAD' ? Path.AbroadAreasSurvey : Path.DomesticAreasSurvey);
+    }
   };
 
   return (
