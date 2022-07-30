8b70735ba2ed13fc2def4fb988af239b61ce2ee3
Jiyoung Kim <kimm.jeongs@gmail.com>
2022-07-30 18:26:37 +0900

    fix: 설문하러가기 연결

--- src/components/domain/matching/buttons/NoneButton.tsx
+++ src/components/domain/matching/buttons/NoneButton.tsx
@@ -1,9 +1,12 @@
 import { Button } from '@/components/base';
 import React from 'react';
+import Path from '@/router/Path';
+import { useNavigate } from 'react-router-dom';
 
 function NoneButton() {
+  const navigate = useNavigate();
   const handleClick = () => {
-    console.log('asd');
+    navigate(Path.TypeOfMeetingSurvey);
   };
   return (
     <Button onClick={handleClick} size="medium" variant={'default'}>
