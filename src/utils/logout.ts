0ea1127090316b62d472aecdc14d6cb3e0c72473
이지호 <cctv1998@naver.com>
2022-08-03 23:57:27 +0900

    feat: 로그아웃 로직 추가

--- src/utils/logout.ts
+++ src/utils/logout.ts
@@ -1,6 +1,9 @@
+import { postWithdraw } from '@/lib/api/user';
 import Cookies from 'js-cookie';
 
-export function logout() {
-  window.location.replace('https://lonessum.com/');
+export async function logout() {
+  await postWithdraw();
   Cookies.remove('AccessToken');
+  Cookies.remove('authenticated');
+  window.location.replace('https://lonessum.com/');
 }
