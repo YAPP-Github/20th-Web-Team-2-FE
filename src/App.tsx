11d68a2762a8dceb94510aa8f3d69f173a166e49
이지호 <cctv1998@naver.com>
2022-08-04 19:58:34 +0900

    Feat: 구글 애널리틱스 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import GlobalStyles from './lib/styles/globalStyles';
 import { ThemeProvider } from 'styled-components';
 import { palette } from '@/lib/styles/palette';
 import Routing from '@/router/Routing';
+import RouteChangeTracker from './utils/RouteChangeTracker';
 
 function App() {
+  RouteChangeTracker();
   return (
     <ThemeProvider theme={{ palette }}>
       <GlobalStyles />
--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,1 +1,2 @@
 export const SERVER_URL = import.meta.env.VITE_SERVER_URL;
+export const TRACKING_ID = import.meta.env.VITE_GOOGLE_GA_ID;
