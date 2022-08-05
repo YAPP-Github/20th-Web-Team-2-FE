9c4eef5f2a5ae789d84721b2899b9c6a34ab3aad
이지호 <cctv1998@naver.com>
2022-08-05 15:21:05 +0900

    Fix : 에러처리 로직변경

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { SERVER_URL } from '@/lib/constants';
 import Cookies from 'js-cookie';
-import { logout } from '@/utils/logout';
 import surveyStorage from '@/utils/surveyStorage';
+import { resetAuth } from '@/utils/logout';
 
 const apiClient = axios.create({
   baseURL: SERVER_URL,
@@ -33,14 +33,11 @@ apiClient.interceptors.response.use(
     switch (response.data.code) {
       case 'INVALID_JWT':
         alert('로그인 시간이 만료되었습니다. 메인으로 이동합니다. 다시 로그인 해주세요!');
-        await logout();
+        await resetAuth();
         break;
       case 'INACTIVE_USER':
         alert('비정상적인 유저입니다.');
-        Cookies.remove('AccessToken');
-        Cookies.remove('authenticated');
-        surveyStorage.remove();
-        window.location.replace('https://lonessum.com/');
+        await resetAuth();
         break;
       default:
         throw new Error(response.data.message);
