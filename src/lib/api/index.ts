cef0f720048b696fdef9fd0a9bdf2ff1210b1492
이지호 <cctv1998@naver.com>
2022-08-05 21:55:38 +0900

    Fix: 오류로 인한 임시 alert 제거

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import { SERVER_URL } from '@/lib/constants';
 import Cookies from 'js-cookie';
-import surveyStorage from '@/utils/surveyStorage';
 import { resetAuth } from '@/utils/logout';
 
 const apiClient = axios.create({
@@ -32,11 +31,9 @@ apiClient.interceptors.response.use(
     console.log(response.data.message);
     switch (response.data.code) {
       case 'INVALID_JWT':
-        alert('로그인 시간이 만료되었습니다. 메인으로 이동합니다. 다시 로그인 해주세요!');
         await resetAuth();
         break;
       case 'INACTIVE_USER':
-        alert('비정상적인 유저입니다.');
         await resetAuth();
         break;
       default:
